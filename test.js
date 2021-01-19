e09ede27097e7237de1b73aaebf8623c90bc1275
David Gallant <kaydeng@nbnet.nb.ca>
2021-01-19 13:23:42 -0400

    Updated package and deps

--- test.js
+++ test.js
@@ -1,12 +1,17 @@
-const dlib = require("./index").default;
+const fs   = require("fs-extra");
+const path = require("path");
+const _    = require("lodash");
 
-console.log("UUID:", dlib.UUID());
+// const dlib = require("./index").default;
+//
+// console.log("UUID:", dlib.UUID());
+//
+// let d = new Date("2019 12 01 22:00:00").getTime()
+//
+// console.log("date:", d, new Date(d  - (new Date().getTimezoneOffset() * 1000 * 60)));
+// console.log("start:", dlib.startOfDay(d), new Date(dlib.startOfDay(d  - (new Date().getTimezoneOffset() * 1000 * 60))));
+// console.log("end:", dlib.endOfDay(d), new Date(dlib.endOfDay(d  - (new Date().getTimezoneOffset() * 1000 * 60))));
+// console.log("countries:", dlib.getCountries());
+//
+// console.log(dlib.numberToIPv4(1521677567));
 
-let d = new Date("2019 12 01 22:00:00").getTime()
-
-console.log("date:", d, new Date(d  - (new Date().getTimezoneOffset() * 1000 * 60)));
-console.log("start:", dlib.startOfDay(d), new Date(dlib.startOfDay(d  - (new Date().getTimezoneOffset() * 1000 * 60))));
-console.log("end:", dlib.endOfDay(d), new Date(dlib.endOfDay(d  - (new Date().getTimezoneOffset() * 1000 * 60))));
-console.log("countries:", dlib.getCountries());
-
-console.log(dlib.numberToIPv4(1521677567));
